e868cb49f0b3924ff1ac956566a5613e67a9ce05
chapkovski <chapkovski@gmail.com>
2017-06-23 01:48:34 -0400

    credentials

--- app1/models.py
+++ app1/models.py
@@ -4,10 +4,10 @@ from otree.api import (
 )
 
 
-author = 'Your name here'
+author = 'Philip Chapkovski, UZH'
 
 doc = """
-Your app description
+Custom WaitPage to push players based on status of players in previous apps
 """
 
 
